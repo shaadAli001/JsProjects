0ee4cadf6ab61bd173b6881a7f91008e20185f13
shaadAli001 <alishaad908@gmail.com>
2025-12-04 11:42:23 +0530

    commit

--- Password_Generator/script.js
+++ Password_Generator/script.js
@@ -1,16 +1,45 @@
-const input_box = document.querySelector("#input_password");
-const lenght = 12;
-const copy = document.querySelector("#copy");
+// const input_box = document.querySelector("#input_password");
+// const lenght = 12;
+// const copy = document.querySelector("#copy");
+
+// let UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+// let NUMBERS = "0123456789";
+// let SYMBOLS = "!@#$%^&*()_-+=<>?/{}[]|~";
+
+// const allChar = UPPERCASE + LOWERCASE + NUMBERS + SYMBOLS;
+
+// const genPassword = () => {
+//   let password = "";
+//   password += UPPERCASE[Math.floor(Math.random() * UPPERCASE.length)];
+//   password += LOWERCASE[Math.floor(Math.random() * LOWERCASE.length)];
+//   password += NUMBERS[Math.floor(Math.random() * NUMBERS.length)];
+//   password += SYMBOLS[Math.floor(Math.random() * SYMBOLS.length)];
+
+//   while (lenght > password.length) {
+//     password += allChar[Math.floor(Math.random() * allChar.length)];
+//   }
+//   input_box.value = password;
+// };
+
+// copy.addEventListener("click", () => {
+//   input_box.select();
+//   document.execCommand("copy");
+// });
 
 let UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
 let NUMBERS = "0123456789";
 let SYMBOLS = "!@#$%^&*()_-+=<>?/{}[]|~";
 
-const allChar = UPPERCASE + LOWERCASE + NUMBERS + SYMBOLS;
+let allChar = UPPERCASE + LOWERCASE + NUMBERS + SYMBOLS;
+
+let lenght = 12;
+const input_password = document.getElementById("input_password");
 
 const genPassword = () => {
   let password = "";
+
   password += UPPERCASE[Math.floor(Math.random() * UPPERCASE.length)];
   password += LOWERCASE[Math.floor(Math.random() * LOWERCASE.length)];
   password += NUMBERS[Math.floor(Math.random() * NUMBERS.length)];
@@ -19,11 +48,10 @@ const genPassword = () => {
   while (lenght > password.length) {
     password += allChar[Math.floor(Math.random() * allChar.length)];
   }
-  input_box.value = password;
+  input_password.value = password;
 };
 
-copy.addEventListener("click", () => {
-  input_box.select();
+const copy = () => {
+  input_password.select();
   document.execCommand("copy");
-});
-
+};
